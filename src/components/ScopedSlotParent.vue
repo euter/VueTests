2e687762544709ca7461a502c722b933b251e048
Admin <01@Hong006.(none)>
2017-10-20 17:21:33 +0800

    init

---
+++ src/components/ScopedSlotParent.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <p>Test scoped slot</p>
+  <awesome-list :items="items">
+    <!-- scoped slot can be named too -->
+    <li
+      slot="item-idx"
+      slot-scope="props">
+      {{props.index+1}}.  {{ props.item.text }}
+    </li>
+  </awesome-list>
+</div>
+</template>
+<script>
+import AwesomeList from './ScopedSlots'
+
+export default {
+  name: 'ListWithScopedSlot',
+  data () {
+    return {
+      items: [{text: 'sea'}, {text: 'fly'}, {text: 'air'}]
+    }
+  },
+  components: {
+    AwesomeList
+  }
+}
+</script>
